ea01a5c9225b55fa018968087e2337d91ee29a06
hjzhong89 <dev8>
2020-11-09 08:36:52 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+
+
+const apiRouter = require('./routes/api');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors());
+
+app.use('/api', apiRouter);
+
+module.exports = app;
